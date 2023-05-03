54033e64a0cc494e68da795f01719d7040ee7551
Rae Gebhart <raegebhart@gmail.com>
2023-05-03 12:49:40 -0600

    Create Login form

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./LoginForm.css";
+
+export const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <form>
+        <label>
+          Email: 
+          <input
+            type="text"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+        </label>
+        <label>
+          Password: 
+          <input
+            type="text"
+            value={password}
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </label>
+      </form>
+    </>
+  );
+};
