dfd3a6f2cbb749a9e1905e45e551308b6c82c1b0
Rae Gebhart <raegebhart@gmail.com>
2023-05-23 16:22:10 -0600

    Add routes to login and signup pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../Home/Home";
+import { Header } from '../Header/Header'
+import { LoginForm } from "../LoginForm/LoginForm";
+import { SignUpForm } from "../SignUpForm/SignupForm";
 
 function App() {
   return (
-    <main>
+    <>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<LoginForm />}></Route>
+        <Route path="/sign-up" element={<SignUpForm />}></Route>
       </Routes>
-    </main>
+    </>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,14 @@
 import { LoginForm } from "../LoginForm/LoginForm"
 import { SignUpForm } from "../SignUpForm/SignupForm"
 
+
 export const Home = () => {
   // consider making state here for currently logged in user
 
   return (
-    <>
-      <LoginForm />
-      {/* <SignUpForm /> */}
-    </>
+    <main className="home-page" >
+      
+
+    </main>
   )
 }
