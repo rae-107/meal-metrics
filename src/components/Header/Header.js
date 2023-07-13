c6dd3ed1d6407d7cfd1daf77c33313d7bd5a66ef
Rae Gebhart <raegebhart@gmail.com>
2023-07-13 13:45:10 -0600

    Display user's first name or log in and sign up buttons depending on if there is a current user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,28 @@
 import "./Header.css";
 import { useNavigate } from "react-router-dom";
 
-export const Header = () => {
+export const Header = ({ currentUser }) => {
   const navigate = useNavigate();
+  let firstName;
+  if (currentUser) {
+    firstName = currentUser?.first_name[0].toUpperCase() + currentUser.first_name.slice(1);
+  }
   return (
     <section className="header">
       <h1 onClick={() => navigate("/")} className="meal-metrics">
         Meal Metrics
       </h1>
-      <div className="account-container">
-        <button onClick={() => navigate("/login")}>Log In</button>
-        <button onClick={() => navigate("/sign-up")}>Sign Up</button>
-      </div>
+      {firstName && (
+        <div className="user-dropdown">
+          <span className="first_name">{firstName}'s Account</span>
+        </div>
+      )}
+      {!firstName && (
+        <div className="account-container">
+          <button onClick={() => navigate("/login")}>Log In</button>
+          <button onClick={() => navigate("/sign-up")}>Sign Up</button>
+        </div>
+      )}
     </section>
   );
 };
