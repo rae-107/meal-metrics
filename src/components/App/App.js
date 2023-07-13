149af8038bd2c45876b282d09bc5f47812b9fe9b
Rae Gebhart <raegebhart@gmail.com>
2023-07-13 13:43:23 -0600

    Add functionality to login and signup while sending the appropiate data down to Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,55 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "../Home/Home";
 import { LoginForm } from "../LoginForm/LoginForm";
 import { SignUpForm } from "../SignUpForm/SignupForm";
+import { Header } from "../Header/Header";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [sessionId, setSessionId] = useState("");
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    fetchData().then((data) => console.log("useEffect", data));
+  }, []);
+
+  const fetchData = async () => {
+    const localStorageSessionId = localStorage.getItem("meal_metrics_sessionId");
+    console.log("app line 19", localStorageSessionId);
+    if (localStorageSessionId) {
+      setSessionId(localStorageSessionId);
+      const res = await fetch(`http://localhost:8080/user/${localStorageSessionId}`);
+      const data = await res.json();
+      setCurrentUser(data)
+      console.log("app line 24", data);
+      return data;
+    }
+  };
+
   return (
     <>
+      <Header currentUser={currentUser} />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<LoginForm />}></Route>
-        <Route path="/sign-up" element={<SignUpForm />}></Route>
+        <Route
+          path="/login"
+          element={
+            <LoginForm
+              sessionId={sessionId}
+              setSessionId={setSessionId}
+              setCurrentUser={setCurrentUser}
+            />
+          }
+        ></Route>
+        <Route
+          path="/sign-up"
+          element={
+            <SignUpForm
+              sessionId={sessionId}
+              setSessionId={setSessionId}
+              setCurrentUser={setCurrentUser}
+            />
+          }
+        ></Route>
       </Routes>
     </>
   );
